7247a443f4807e2438836596beeb0a82ebd983a1
punxe <dsammsh@gmail.com>
2022-10-22 15:46:11 -0700

    Created Spring Boot starter project

---
+++ src/main/java/io/dubconnects/dubconnects/DubconnectsApplication.java
@@ -0,0 +1,13 @@
+package io.dubconnects.dubconnects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubconnectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DubconnectsApplication.class, args);
+	}
+
+}
