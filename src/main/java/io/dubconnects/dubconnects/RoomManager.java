e0dcb32937562956241a2d873cf27c8a97566ef7
punxe <dsammsh@gmail.com>
2022-10-22 22:38:32 -0700

    Added Queue system to RoomManager

--- src/main/java/io/dubconnects/dubconnects/RoomManager.java
+++ src/main/java/io/dubconnects/dubconnects/RoomManager.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.twilio.Twilio;
 import com.twilio.jwt.accesstoken.AccessToken;
 import com.twilio.jwt.accesstoken.VideoGrant;
+import com.twilio.rest.video.v1.Room;
 
 @RestController
 public class RoomManager {
@@ -19,18 +20,31 @@ public class RoomManager {
     @Autowired
     private TwilioCredentialsManager credentials;
 
+    private Queue<Room> rooms = new LinkedList<>();
+
+    long rollingRoomId = 0;
+
     public RoomManager() {
 
     }
 
-    @GetMapping(path = "/rooms/{user}", produces = "application/json")
-    public String joinRoom(@PathVariable String user) {
-        VideoGrant grant = new VideoGrant().setRoom("placeholder");
+    @GetMapping("/rooms/join")
+    public String joinRoom() {
+        Room room;
+        if(!rooms.isEmpty()){
+            room = rooms.poll();
+        }else{
+            room = Room.creator().setUniqueName("" + rollingRoomId).create();
+
+            rooms.add(room);
+            rollingRoomId++;
+        }
+        VideoGrant grant = new VideoGrant().setRoom(room.getUniqueName());
         AccessToken token = new AccessToken.Builder(
                 credentials.getAccountSid(),
                 credentials.getKeySid(),
                 credentials.getKeySecret()
-        ).identity(user).grant(grant).build();
+        ).identity("user").grant(grant).build();
 
         return token.toJwt();
     }
