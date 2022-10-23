28fd092e2db2ca1b877330041c92ec9fd92d443a
Sifat Hasan <sifat120@outlook.com>
2022-10-23 08:52:46 -0700

    Added all design elements for the landing page
    Design elements can be found in App.scss. Make sure to run npm install as well, if there are added libraries. Otherwise, run npm install scss --save or npm i node-scss

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,10 +1,19 @@
+import './App.scss'
 import React from 'react';
 import LandingPage from './pages/LandingPage';
+import Logo from './img/Logo.png';
 import MainPage from './pages/MainPage';
 
 function App() {
   return (
+<<<<<<< Updated upstream
     <MainPage />
+=======
+    <div className="App">
+      <img src={Logo} alt='Logo' />
+      <LandingPage />
+    </div>
+>>>>>>> Stashed changes
   )
 }
 
--- src/frontend/src/pages/LandingPage.js
+++ src/frontend/src/pages/LandingPage.js
@@ -1,8 +1,51 @@
 import React from 'react'
 import Background from '../components/Background'
 
+<<<<<<< Updated upstream
 function buttonClicked() {
   alert('Button clicked!')
+=======
+class LandingPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      room: false,
+      identifier: ''
+    };
+  }
+
+  buttonClicked() {
+    this.setState({ room: true });
+    alert('Button clicked!')
+    console.log('Button clicked!');
+  }
+
+  removePlaceholderText() {
+    this.inputRef.current.placeholder = '';
+  }
+
+  updateIdentity(event) {
+    this.setState({ identifier: event.target.value });
+  }
+
+  render() {
+    return (
+      <div className="landingpage">
+        {
+          <div className="lobby">
+            <input
+              value={this.state.identifier}
+              onChange={this.updateIdentity}
+              ref={this.inputRef}
+              onClick={this.removePlaceholderText}
+              placeholder="Enter Your UW NetId (@uw.edu)" />
+            <button onClick={this.buttonClicked}>Join Room</button>
+          </div>
+        }
+      </div>
+    )
+  }
+>>>>>>> Stashed changes
 }
 
 export default function LandingPage() {
